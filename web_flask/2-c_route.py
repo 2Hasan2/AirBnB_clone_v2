edbd82226bf8ff3273ae99735f5df8dbef8a719e
2Hasan2 <h01554176846@gmail.com>
2024-05-16 14:52:14 +0300

    cool

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -14,7 +14,7 @@ def hbnb():
 
 @app.route('/c/<text>', strict_slashes=False)
 def c_route(text):
-    return "C {}".format(text.replace("_", " "))
+    return f"C {text.replace("_", " ")}"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
