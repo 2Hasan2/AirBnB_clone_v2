8bc48d73ca1b38b1c11d76b7a8c7a4c3cfb958a2
2Hasan2 <h01554176846@gmail.com>
2024-05-18 21:06:13 +0300

    2024-05-18 09:06:13 PM

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -45,7 +45,6 @@ def number_template(n):
 def number_odd_or_even(n):
     """Displays an HTML page only if <n> is an integer."""
     return render_template("6-number_odd_or_even.html", n=n)
-    pass
 
 
 if __name__ == "__main__":
