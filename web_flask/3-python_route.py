e0cecb7a60b16e5236a47a7dca766ce1fbd69382
2Hasan2 <h01554176846@gmail.com>
2024-05-18 20:39:39 +0300

    2024-05-18 08:39:39 PM

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""This module starts a Flask web application"""
+import flask
+
+app = flask.Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """Displays 'Hello HBNB!'."""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Displays 'HBNB'."""
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_route(text):
+    """Displays 'C' followed by the value of the text variable."""
+    return f"C {text.replace('_', ' ')}"
+
+
+@app.route('/python/<text>', strict_slashes=False)
+def python(text):
+    return f"Python is {text}"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
