f7de43121e86bed5dcb5bf0944ce817761a0b5ca
2Hasan2 <h01554176846@gmail.com>
2024-05-18 20:33:44 +0300

    2024-05-18 08:33:44 PM

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,21 +1,25 @@
 #!/usr/bin/python3
 """This module starts a Flask web application"""
 import flask
+
 app = flask.Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
 def hello():
+    """Displays 'Hello HBNB!'."""
     return "Hello HBNB!"
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
+    """Displays 'HBNB'."""
     return "HBNB"
 
 
 @app.route('/c/<text>', strict_slashes=False)
 def c_route(text):
+    """Displays 'C' followed by the value of the text variable."""
     return f"C {text.replace('_', ' ')}"
 
 
