cb1eb4184661e72a42177d8f078244694acc1932
2Hasan2 <h01554176846@gmail.com>
2024-05-18 21:08:41 +0300

    2024-05-18 09:08:41 PM

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -24,7 +24,7 @@ def c_route(text):
 
 
 @app.route('/python/<text>', strict_slashes=False)
-def python(text):
+def python(text="is cool"):
     """Displays 'Python is' followed by the value of the text variable."""
     return f"Python is {text}"
 
