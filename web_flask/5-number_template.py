b50c73875364edecd0e8a3bd6a1a59f11dfaeccc
2Hasan2 <h01554176846@gmail.com>
2024-05-18 20:51:27 +0300

    2024-05-18 08:51:27 PM

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""This module starts a Flask web application"""
+import flask
+
+app = flask.Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """Displays 'Hello HBNB!'."""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Displays 'HBNB'."""
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_route(text):
+    """Displays 'C' followed by the value of the text variable."""
+    return f"C {text.replace('_', ' ')}"
+
+
+@app.route('/python/<text>', strict_slashes=False)
+def python(text):
+    return f"Python is {text}"
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    return f"{n} is a number"
+
+
+@app.route('number_template/<int:n>', strict_slashes=False)
+def number_template(n):
+    return flask.render_template("5-number.html", n)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
